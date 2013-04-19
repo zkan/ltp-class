05b64bf14508706c5c2b61338e2eaa63a03b4132
Kan O <zkan.cs@gmail.com>
2013-04-19 19:12:27 +0700

    add tests for Maze

---
+++ LTP-CraftingQualityCode/assignment_02/test_maze.py
@@ -0,0 +1,65 @@
+import a2
+import unittest
+
+class TestMazeMethods(unittest.TestCase): 
+    """ Test class for methods in class Maze. """ 
+
+    def test_init_maze(self): 
+        """ Test __init__ with a given maze, rat_1, and rat_2. """
+
+        m = a2.Maze([['#', '#', '#', '#', '#', '#', '#'], 
+                     ['#', '.', '.', '.', '.', '.', '#'], 
+                     ['#', '.', '#', '#', '#', '.', '#'], 
+                     ['#', '.', '.', '@', '#', '.', '#'], 
+                     ['#', '@', '#', '.', '@', '.', '#'], 
+                     ['#', '#', '#', '#', '#', '#', '#']], 
+                     a2.Rat('J', 1, 1),
+                     a2.Rat('P', 1, 4))
+        expected_maze = [['#', '#', '#', '#', '#', '#', '#'], 
+                         ['#', '.', '.', '.', '.', '.', '#'], 
+                         ['#', '.', '#', '#', '#', '.', '#'], 
+                         ['#', '.', '.', '@', '#', '.', '#'], 
+                         ['#', '@', '#', '.', '@', '.', '#'], 
+                         ['#', '#', '#', '#', '#', '#', '#']]
+        expected_rat_1 = a2.Rat('J', 1, 1)
+        expected_rat_2 = a2.Rat('P', 1, 4)
+        expected_num_sprouts_left = 3
+
+        self.assertEqual(expected_maze, m.maze)
+        self.assertTrue(expected_rat_1.__eq__(m.rat_1))
+        self.assertTrue(expected_rat_2.__eq__(m.rat_2))
+        self.assertEqual(expected_num_sprouts_left, m.num_sprouts_left)
+   
+    def test_is_really_wall(self):
+        """ Test is_wall to check if there is a wall at the given row and column of the maze. 
+        Suppose (0, 0) is given. """
+
+        m = a2.Maze([['#', '#', '#', '#', '#', '#', '#'], 
+                     ['#', '.', '.', '.', '.', '.', '#'], 
+                     ['#', '.', '#', '#', '#', '.', '#'], 
+                     ['#', '.', '.', '@', '#', '.', '#'], 
+                     ['#', '@', '#', '.', '@', '.', '#'], 
+                     ['#', '#', '#', '#', '#', '#', '#']], 
+                     a2.Rat('J', 1, 1),
+                     a2.Rat('P', 1, 4))
+        
+        self.assertTrue(m.is_wall(0, 0))
+
+    def test_is_not_wall(self):
+        """ Test is_wall to check if there is not a wall at the given row and column of the maze. 
+        Suppose (1, 4) is given. """
+
+        m = a2.Maze([['#', '#', '#', '#', '#', '#', '#'], 
+                     ['#', '.', '.', '.', '.', '.', '#'], 
+                     ['#', '.', '#', '#', '#', '.', '#'], 
+                     ['#', '.', '.', '@', '#', '.', '#'], 
+                     ['#', '@', '#', '.', '@', '.', '#'], 
+                     ['#', '#', '#', '#', '#', '#', '#']], 
+                     a2.Rat('J', 1, 1),
+                     a2.Rat('P', 1, 4))
+        
+        self.assertFalse(m.is_wall(1, 4))
+
+
+if __name__ == '__main__':
+    unittest.main(exit=False)
