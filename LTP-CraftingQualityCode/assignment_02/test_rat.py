2c38d279ff94a62150431cf381ca3f2953e8cfa4
Kan O <zkan.cs@gmail.com>
2013-04-19 17:47:42 +0700

    finish __init__

---
+++ LTP-CraftingQualityCode/assignment_02/test_rat.py
@@ -0,0 +1,21 @@
+import a2
+import unittest
+
+class TestInitializeRat(unittest.TestCase): 
+    """ Test class for method a2. """ 
+
+    def test_init_rat(self): 
+        """ Test __init__ with symbol 'P' at the location (1, 4) """
+
+        actual = a2.Rat('P', 1, 4)
+        expected_symbol = 'P'
+        expected_row = 1
+        expected_col = 4
+
+        self.assertEqual(expected_symbol, actual.symbol)
+        self.assertEqual(expected_row, actual.row)
+        self.assertEqual(expected_col, actual.col)
+
+
+if __name__ == '__main__':
+    unittest.main(exit=False)
